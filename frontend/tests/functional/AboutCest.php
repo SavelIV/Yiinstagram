2b2c73dc07f6d1e8f25acdca361528b9ff37f24b
igor Savelev <savelevi@mail.ru>
2020-12-04 03:18:18 +0300

    Some tests added

--- frontend/tests/functional/AboutCest.php
+++ frontend/tests/functional/AboutCest.php
@@ -8,7 +8,7 @@ class AboutCest
     public function checkAbout(FunctionalTester $I)
     {
         $I->amOnRoute('site/about');
-        $I->see('About Yiinstagram project', 'h1');
+        $I->see('About this project', 'h1');
     }
 
 }
